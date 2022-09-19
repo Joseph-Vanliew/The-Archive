345657da977a37b3f474ea973ee92b49aa55edeb
Austin <austinrandall206@gmail.com>
2022-09-19 06:44:45 -0600

    austins branch

---
+++ Application/src/main/java/com/kenzie/appserver/dao/UserDAO.java
@@ -0,0 +1,23 @@
+package com.kenzie.appserver.dao;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.kenzie.appserver.objects.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO {
+    private final DynamoDBMapper mapper;
+
+    public UserDAO(DynamoDBMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    public User getUserById(String userId){
+       User user = new User();
+       user.setUserId(userId);
+       mapper.load(user);
+       return user;
+    }
+
+}
---
+++ Application/src/main/java/com/kenzie/appserver/objects/User.java
@@ -0,0 +1,57 @@
+package com.kenzie.appserver.objects;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+@DynamoDBTable(tableName = "User")
+public class User {
+    private String userId;
+    private String name;
+    private String dateJoined;
+
+    public User(String userId, String name, String dateJoined) {
+        this.userId = userId;
+        this.name = name;
+        this.dateJoined = dateJoined;
+    }
+    public User(){
+    }
+
+    @DynamoDBHashKey(attributeName = "userId")
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @DynamoDBAttribute(attributeName = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @DynamoDBRangeKey(attributeName = "timeStamp")
+    public String getDateJoined() {
+        return dateJoined;
+    }
+
+    public void setDateJoined(String dateJoined) {
+        this.dateJoined = dateJoined;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", name='" + name + '\'' +
+                ", dateJoined='" + dateJoined + '\'' +
+                '}';
+    }
+}
